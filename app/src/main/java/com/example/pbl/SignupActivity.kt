9e030a654faaa58b240be2ae6814e04abe3916c6
s21011 <s21011@std.it-college.ac.jp>
2023-08-15 00:38:42 +0900

    データベースを少し修正

--- app/src/main/java/com/example/pbl/SignupActivity.kt
+++ app/src/main/java/com/example/pbl/SignupActivity.kt
@@ -1,9 +1,14 @@
 package com.example.pbl
 
+import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pbl.databinding.ActivitySignupBinding
+import java.util.jar.Attributes.Name
 
 class SignupActivity : AppCompatActivity(){
     private lateinit var binding: ActivitySignupBinding
@@ -12,6 +17,26 @@ class SignupActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // まだ未完成
+        val helper = SimpleDatabaseHelper(this)
+        val Name = findViewById<EditText>(R.id.Name)
+        val Password = findViewById<EditText>(R.id.Password)
+
+        findViewById<Button>(R.id.signupregibtn).setOnClickListener {
+            helper.writableDatabase.use { db ->
+                val cv = ContentValues().apply {
+                    put("name", Name.text.toString())
+                    put("password", Password.text.toString())
+                }
+                db.insert("signup", null, cv)
+                Toast.makeText(
+                    this, "登録しました",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        //
 
         binding.signupregibtn.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
